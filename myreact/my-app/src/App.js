b401c7ea48c01715df091670cb4bdc709dec3645
JiaLei <sfw123817@gmail.com>
2018-06-24 13:12:01 +0800

    add simple update

--- myreact/my-app/src/App.js
+++ myreact/my-app/src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    var a = "234";
+    var b = 123
+    var scope = "golab var"
+
+    function test(){
+      document.writeln(scope + "<br />")
+      var scope = "local var"
+      document.writeln(scope + "<br />")
+    }
+    test();
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +23,37 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>{a + b}</h2>
+        <GroceryList />
       </div>
     );
   }
 }
+
+class ListItem extends Component {
+  render() {
+    return(
+      <li>
+        {this.propos}
+      </li>
+    )
+  }
+}
+
+class GroceryList extends Component{
+  render() {
+    return (
+      <ul>
+        <ListItem quantity="1" name="Bread" />
+        <ListItem quantity="6" name="Eggs" />
+        <ListItem quantity="2" name="Milk" />
+      </ul>
+    );
+  }
+}
+
+
+
+
 
 export default App;
