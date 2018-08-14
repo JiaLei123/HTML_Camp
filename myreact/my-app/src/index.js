15e4c87d39e39af7948ee0feea1ad1fce164d16b
JiaLei <sfw123817@gmail.com>
2018-08-14 23:34:07 +0800

    redux_container

--- myreact/my-app/src/index.js
+++ myreact/my-app/src/index.js
@@ -6,7 +6,8 @@ import KanBan from './kanban/KanBan';
 import ClickCounter from './deep_re/ClickCounter'
 // import ControlPane from './deep_re/ControlPanel'
 // import ControlPane from './flux/view/ControlPanel';
-import ControlPane from './redux1/view/ControlPanel';
+// import ControlPane from './redux1/view/ControlPanel';
+import ControlPane from './redux2/view/ControlPanel';
 import Acc_Scatter from './dashboard/acc_satter';
 import registerServiceWorker from './registerServiceWorker';
 
