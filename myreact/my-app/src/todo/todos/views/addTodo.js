f9a825f96db5b81ef6ff5343e7345fa4deaeb793
JiaLei <sfw123817@gmail.com>
2018-08-20 23:57:05 +0800

    update

--- myreact/my-app/src/todo/todos/views/addTodo.js
+++ myreact/my-app/src/todo/todos/views/addTodo.js
@@ -6,16 +6,47 @@ import {addTodo} from '../Action';
 class AddTodo extends Component {
     constructor(props, context) {
         super(props, context);
-
-        this.state = {
-            value: ''
-        };
     }
 
     onSubmit(ev){
         ev.preventDefault();
+        const input = this.input;
+        if(!input.value.trim()){
+            return;
+        }
 
+        this.props.onAdd(input.node);
+        input.value="";
+    }
+    
+    refInput(node){
+        this.input = node;
     }
 
+    render() {
+        return(
+            <div className="add-todo">
+                <form onSubmit={this.onSubmit}>
+                    <input className="new-todo" ref={this.refInput} />
+                    <button className="add-btn" type="submit">
+                        添加
+                    </button>
+                </form>
+            </div>
+        )
+    }
 
 }
+
+AddTodo.propTypes = {
+    onAdd: PropTypes.func.isRequired
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onAdd: (text) =>{
+            dispatch(addTodo(text))
+        }
+    }
+}
+  
