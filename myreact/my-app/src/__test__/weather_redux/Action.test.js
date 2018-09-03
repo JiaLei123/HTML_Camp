9910ebe4d39a1456579375a783ac08b630970b66
JiaLei <sfw123817@gmail.com>
2018-09-04 00:02:01 +0800

    add test case

--- myreact/my-app/src/__test__/weather_redux/Action.test.js
+++ myreact/my-app/src/__test__/weather_redux/Action.test.js
@@ -1,11 +1,33 @@
-import moduleName from '../../weather_redux/weather/actions';
+import fetchWeather from '../../weather_redux/weather/actions';
 import {FETCH_STARTED, FETCH_SUCCESS, FETCH_FAILURE} from '../../weather_redux/weather/actionTypes';
 import {stub} from 'sinon';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+
+const middlewares = [thunk]
+const createMockStore = configureStore(middlewares)
 
 describe("fetchwetcher", ()=>{
     let stubbedFetch;
     beforeEach(() =>{
         stubbedFetch = stub(global, 'fetch');
     });
+    afterEach(() =>{
+        stubbedFetch.restore();
+    })
 
+    it("should dispatch featchWeather", ()=>{
+        const mockResponse = Promise.resolve({
+            status:200,
+            json: ()=> Promise.resolve({
+                weatherinfo: {}
+            })
+        });
+
+        stubbedFetch.returns(mockResponse);
+        return createMockStore.dispatch(fetchWeather(1)).then(()=>{
+            const dispatchedActions = store.getActions();
+            // expect(dispatchedActions.length).toBe(2);
+        });
+    })
 })
